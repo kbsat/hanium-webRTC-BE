6208e098d82e92d8193d06b690dcb9dfb33aaafd
junbeom-kang <kgood10369@gmail.com>
2021-07-15 02:13:54 +0900

    spring initializr를 통한 프로젝트 생성

---
+++ src/main/java/hanium/videoMeetintg/VideoMeetintgApplication.java
@@ -0,0 +1,13 @@
+package hanium.videoMeetintg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoMeetintgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoMeetintgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanium/videoMeetintg/VideoMeetintgApplicationTests.java
@@ -0,0 +1,13 @@
+package hanium.videoMeetintg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoMeetintgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
