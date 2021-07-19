d6c9035d23b3b441a6bb9749c1702bd52dffb1c8
junbeom-kang <kgood10369@gmail.com>
2021-07-19 23:57:07 +0900

    reservation 엔티티 개발

---
+++ src/main/java/hanium/videoMeetintg/domain/Reservation.java
@@ -0,0 +1,25 @@
+package hanium.videoMeetintg.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "host_id")
+    private User host_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room_id;
+
+    private LocalDateTime reservation_time;
+}
