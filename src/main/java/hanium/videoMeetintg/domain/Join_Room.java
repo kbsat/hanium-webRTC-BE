669c5c5335fa1f5c50d32fc0079c2ec0eb1ecda1
junbeom-kang <kgood10369@gmail.com>
2021-07-19 23:52:56 +0900

    joinRoom 엔티티 개발

---
+++ src/main/java/hanium/videoMeetintg/domain/Join_Room.java
@@ -0,0 +1,21 @@
+package hanium.videoMeetintg.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Join_Room {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="join_room_id")
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user_id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="room_id")
+    private Room room_id;
+}
