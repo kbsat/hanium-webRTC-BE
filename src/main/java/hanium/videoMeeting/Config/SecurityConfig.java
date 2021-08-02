f7c297eb3f990b20535464711e90bf3194ab9036
junbeom-kang <kgood10369@gmail.com>
2021-08-02 22:29:03 +0900

    config 파일 추가

--- src/main/java/hanium/videoMeeting/Config/SecurityConfig.java
+++ src/main/java/hanium/videoMeeting/Config/SecurityConfig.java
@@ -2,12 +2,22 @@ package hanium.videoMeeting.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
     }
 }
