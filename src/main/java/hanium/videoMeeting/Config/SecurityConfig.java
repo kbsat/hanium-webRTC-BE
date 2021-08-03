9d7eb7450cf670ecea2e74c2eaf75e1c76ff9864
junbeom-kang <kgood10369@gmail.com>
2021-08-03 12:24:26 +0900

    JWT토큰을 위한 의존성 추가 및 Config 파일 설정

--- src/main/java/hanium/videoMeeting/Config/SecurityConfig.java
+++ src/main/java/hanium/videoMeeting/Config/SecurityConfig.java
@@ -1,16 +1,23 @@
 package hanium.videoMeeting.Config;
 
+import hanium.videoMeeting.Config.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CorsConfig corsConfig;
+    private final TokenProvider tokenProvider;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -18,6 +25,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http
+                .addFilter(corsConfig.corsFilter())
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
     }
 }
