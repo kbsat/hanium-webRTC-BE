f2dd9814cecdc585b877962b831f9a5dc2cdc896
BeomsooKim <kbsat@naver.com>
2021-09-27 16:12:04 +0900

    메일 전송 기능 추가. 예약된 방 시간되면 메일 전송

--- src/main/java/hanium/videoMeeting/repository/RoomRepository.java
+++ src/main/java/hanium/videoMeeting/repository/RoomRepository.java
@@ -4,7 +4,9 @@ import hanium.videoMeeting.domain.Room;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,4 +28,11 @@ public interface RoomRepository extends JpaRepository<Room,Long> {
             "   and r.password = ''" +
             " order by r.id desc")
     long countPublicRoom();
+
+    @Query("select r from Room r" +
+            " join fetch r.host h" +
+            " where r.isReserved = true" +
+            "   and r.start_time between :start and :end")
+    List<Room> findReservedRoomsBetweenTime(@Param("start") LocalDateTime start,@Param("end") LocalDateTime end);
+
 }
